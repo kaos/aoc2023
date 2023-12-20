dcabc6ce51adb4b74c130b51dd8fcd59f648ef6b
Andreas Stenius <andreas.stenius@imanage.com>
2023-12-20 17:21:08 -0500

    day 2, step 2.

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -14,17 +14,19 @@ fn main() {
     ("blue".to_owned(), 14),
   ]);
 
-  let res: u32 = contents.split("\n").map(parse_game)
-    .filter_map(|(game_id, shown_cubes): (u32, Vec<(u32, String)>)| {
-      if shown_cubes.iter().all(|(count, color): &(u32, String)| count <= &cubes[color]) {
-        Some(game_id)
-      } else {
-        None
-      }
-    })
+  let calc = possible_games(&cubes);
+  let res: u32 = contents.split("\n")
+    .map(parse_game)
+    .filter_map(calc)
     .sum();
+  println!("Result 1: {res:?}");
 
-  println!("Result: {res:?}");
+  let calc = game_power;
+  let res: u32 = contents.split("\n")
+    .map(parse_game)
+    .filter_map(calc)
+    .sum();
+  println!("Result 2: {res:?}");
 }
 
 
@@ -45,4 +47,27 @@ fn parse_cubes(game: &str) -> Vec<(u32, String)> {
       _ => None
     }
   }).collect()
+}
+
+
+fn possible_games(cubes: &HashMap::<String, u32>) -> impl Fn((u32, Vec<(u32, String)>)) -> Option<u32> + '_ {
+  |(game_id, shown_cubes)| {
+    if shown_cubes.iter().all(|(count, color): &(u32, String)| { count <= &cubes[color] }) {
+      Some(game_id)
+    } else {
+      None
+    }
+  }
+}
+
+
+fn game_power((_, shown_cubes): (u32, Vec<(u32, String)>)) -> Option<u32> {
+  let mut stats = HashMap::<String, u32>::from([("red".to_owned(), 0), ("green".to_owned(), 0), ("blue".to_owned(), 0)]);
+  for (count, color) in shown_cubes.iter() {
+    if count > &stats[color] {
+      stats.insert(color.to_string(), *count);
+    }
+  }
+
+  Some(stats.values().fold(1, |acc, value| acc * value))
 }
